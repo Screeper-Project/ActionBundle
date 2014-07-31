65d805369b1c47fba1e7ea0f175f4bb89d4661ad
Thibault Piana <astres3000@free.fr>
2014-07-31 23:02:03 +0200

    Update ActionsListCommand.php

--- Command/ActionsListCommand.php
+++ Command/ActionsListCommand.php
@@ -30,10 +30,7 @@ class ActionsListCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if($input->getArgument('server'))
-            $server = $input->getArgument('server');
-        else
-            $server = ServerService::DEFAULT_SERVER_KEY;
+        $server = ($input->getArgument('server')) ? $input->getArgument('server') : ServerService::DEFAULT_SERVER_KEY;
 
         $container = $this->getContainer();
         $checkConnection = $container->get('screeper.json_api.services.api')->getServerStatus($server);
